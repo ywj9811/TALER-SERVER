2db0bc380514e0e6d6442cdce13cac39038918e2
양원준 <97458548+ywj9811@users.noreply.github.com>
2023-01-03 23:38:18 +0900

    Initial commit

---
+++ src/main/java/com/demo/TalerApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalerApplication.class, args);
+	}
+
+}
