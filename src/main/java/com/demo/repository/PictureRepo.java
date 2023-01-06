bc4b3878580717c7821ddfe3b6e64532c27a61d2
양원준 <97458548+ywj9811@users.noreply.github.com>
2023-01-07 05:16:20 +0900

    feat: bookroomId를 통한 조회 추가

---
+++ src/main/java/com/demo/repository/PictureRepo.java
@@ -0,0 +1,12 @@
+package com.demo.repository;
+
+import com.demo.domain.Picturetable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PictureRepo extends JpaRepository<Picturetable, Long> {
+    List<Picturetable> findAllByBookroomId(Long bookroomId);
+}
