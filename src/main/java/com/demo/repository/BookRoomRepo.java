ccccb6aed8105c15c7702c7fe1d5adbd3da7b978
양원준 <97458548+ywj9811@users.noreply.github.com>
2023-01-07 05:00:39 +0900

    fix: 주석 처리

--- src/main/java/com/demo/repository/BookRoomRepo.java
+++ src/main/java/com/demo/repository/BookRoomRepo.java
@@ -1,6 +1,5 @@
 package com.demo.repository;
 
-import com.demo.domain.BookRoomPlusBookDetails;
 import com.demo.domain.Bookroom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,7 +11,7 @@ public interface BookRoomRepo extends JpaRepository<Bookroom, Long> {
 
     Bookroom findByUserIdAndBookId(Long bookId, Long userId);
 
-    @Query("select bd, br from Bookdetails bd JOIN Bookroom br ON br.bookId = br.bookId where br.userId = :userId")
-    BookRoomPlusBookDetails findBookTitle(@Param("userId") Long userId);
+//    @Query("select bd, br from Bookdetails bd JOIN Bookroom br ON br.bookId = br.bookId where br.userId = :userId")
+//    BookRoomPlusBookDetails findBookTitle(@Param("userId") Long userId);
     
 }
