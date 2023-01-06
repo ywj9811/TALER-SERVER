9911352bc662c80fd204b7582516354f30c23609
양원준 <97458548+ywj9811@users.noreply.github.com>
2023-01-07 05:39:20 +0900

    feat: word 탭을 위한 조회 구현

--- src/main/java/com/demo/repository/WordRepo.java
+++ src/main/java/com/demo/repository/WordRepo.java
@@ -4,6 +4,9 @@ import com.demo.domain.Wordtable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface WordRepo extends JpaRepository<Wordtable, Long> {
+    List<Wordtable> findAllByBookroomId(Long bookroomId);
 }
