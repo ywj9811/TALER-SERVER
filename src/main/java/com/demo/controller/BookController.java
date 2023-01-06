f864e30af7d827728808ae555457d6ca4b09c672
양원준 <97458548+ywj9811@users.noreply.github.com>
2023-01-07 04:59:57 +0900

    fix: join문 삭제 후 bookroom과 bookdetails 분리하여 Map으로 반환

--- src/main/java/com/demo/controller/BookController.java
+++ src/main/java/com/demo/controller/BookController.java
@@ -1,7 +1,24 @@
 package com.demo.controller;
 
+import com.demo.service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/book")
 public class BookController {
+    private final BookService bookService;
+
+    @GetMapping("/bookroom")
+    public Map<String, Object> moveToBookRoom(Long userId, Long bookId) {
+//        return bookService.moveToBookRoom(bookId, userId);
+        Map<String, Object> returnMap = bookService.getBookRoom(bookId, userId);
+
+        return returnMap;
+    }
 }
--- src/main/java/com/demo/service/BookService.java
+++ src/main/java/com/demo/service/BookService.java
@@ -1,18 +1,45 @@
 package com.demo.service;
 
+import com.demo.domain.Bookdetails;
 import com.demo.domain.Bookroom;
+import com.demo.repository.BookDetailsRepo;
 import com.demo.repository.BookRoomRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class BookService {
     private final BookRoomRepo bookRoomRepo;
+    private final BookDetailsRepo bookDetailsRepo;
 
-    public Bookroom findByUserIdAndBookId(Long userId, Long bookId) {
-        return bookRoomRepo.findByUserIdAndBookId();
+    /**
+     * 기본 외에 제공해야 할 데이터
+     * 1. bookId를 통한 책 제목
+     * 2. 그리고 그냥 bookroom을 통째로 제공하여도 괜찮을지
+     */
+//    public BookRoomPlusBookDetails moveToBookRoom(Long userId, Long bookId) {
+//        BookRoomPlusBookDetails bookRoomPlusBookDetails = bookRoomRepo.findBookTitle(userId);
+//        return bookRoomPlusBookDetails;
+//    }
+
+    public Map<String, Object> getBookRoom(Long bookId, Long userId) {
+        Map<String, Object> returnMap = new HashMap<>();
+
+        Bookroom bookroom = bookRoomRepo.findByUserIdAndBookId(bookId, userId);
+        Optional<Bookdetails> optionalBookdetails = bookDetailsRepo.findById(bookId);
+        if (optionalBookdetails.isEmpty())
+            return null;
+        Bookdetails bookdetails = optionalBookdetails.get();
+        returnMap.put("bookroom", bookroom);
+        returnMap.put("bookdetails", bookdetails);
+
+        return returnMap;
     }
 }
