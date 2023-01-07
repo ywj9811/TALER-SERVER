d9dd15d045ec9310ad121d7c572a7b9de41bd0ee
양원준 <97458548+ywj9811@users.noreply.github.com>
2023-01-07 18:56:54 +0900

    refactor: Optional로 반환할 수 있도록 수정

--- src/main/java/com/demo/repository/BookRoomRepo.java
+++ src/main/java/com/demo/repository/BookRoomRepo.java
@@ -4,10 +4,12 @@ import com.demo.domain.Bookroom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BookRoomRepo extends JpaRepository<Bookroom, Long> {
 
-    Bookroom findByUserIdAndBookId(Long bookId, Long userId);
+    Optional<Bookroom> findByUserIdAndBookId(Long bookId, Long userId);
 
 //    @Query("select bd, br from Bookdetails bd JOIN Bookroom br ON br.bookId = br.bookId where br.userId = :userId")
 //    BookRoomPlusBookDetails findBookTitle(@Param("userId") Long userId);
