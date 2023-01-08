0e7e4b615672d346666a5838d0bd6dde40492c36
양원준 <97458548+ywj9811@users.noreply.github.com>
2023-01-08 22:29:46 +0900

    fix: join실패 후 view사용으로 수정 (주석처리)

--- src/main/java/com/demo/domain/BookRoomPlusBookDetails.java
+++ src/main/java/com/demo/domain/BookRoomPlusBookDetails.java
@@ -1,5 +1,23 @@
 package com.demo.domain;
 
 public interface BookRoomPlusBookDetails {
+    /**
+     * 결과값이 안나옴 null로 나옴
+     * 나중에 이유를 알게 되면 고치도록 하자.
+     * 우선 작동하는 방식으로 하자.
+     */
+    Long getBookId();
+    String getBookTitle();
+    String getBookAuthor();
+    String getBookGenre();
+    String getBookPopularity();
 
+    Long getBookroomId();
+    Long getUserId();
+    Long getCharacterId();
+    String getThemeColor();
+    String getThemeMusicUrl();
+
+//    Bookroom getBookroom();
+//    Bookdetails getBookdetails();
 }
