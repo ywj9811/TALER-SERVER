584f3f5326ae7cef2bcf9ca0347e0f0581ff34b5
양원준 <97458548+ywj9811@users.noreply.github.com>
2023-01-10 21:45:20 +0900

    feat: createAt, updateAt 자동 처리 추가

--- src/main/java/com/demo/domain/User.java
+++ src/main/java/com/demo/domain/User.java
@@ -1,11 +1,14 @@
 package com.demo.domain;
 
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.time.LocalDateTime;
 
 @Getter
 @Entity
@@ -23,9 +26,11 @@ public class User {
 
     private String profileColor;
 
-    private String createdAt;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
-    private String updateAt;
+    @UpdateTimestamp
+    private LocalDateTime updateAt;
 
     private int status;
 
