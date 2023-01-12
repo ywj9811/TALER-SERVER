ec39569eaf92c0d0d0dae6797a4d4ddebe044134
양원준 <97458548+ywj9811@users.noreply.github.com>
2023-01-12 16:35:09 +0900

    fix: bookroom 추가할 때 themeColor, themeMusicUrl 제외

--- src/main/java/com/demo/dto/BookRoomInsertDto.java
+++ src/main/java/com/demo/dto/BookRoomInsertDto.java
@@ -9,15 +9,11 @@ import lombok.Data;
 public class BookRoomInsertDto {
     Long userId;
     Long bookId;
-    String themeColor;
-    String themeMusicUrl;
 
     public Bookroom dtoToBookRoom(BookRoomInsertDto bookRoomInsertDto) {
         return Bookroom.builder()
                 .userId(bookRoomInsertDto.getUserId())
                 .bookId(bookRoomInsertDto.getBookId())
-                .themeColor(bookRoomInsertDto.getThemeColor())
-                .themeMusicUrl(bookRoomInsertDto.getThemeMusicUrl())
                 .build();
     }
 }
