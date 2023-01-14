2b5aec7eb1df2f6927caba15216a885bbf5215f2
양원준 <97458548+ywj9811@users.noreply.github.com>
2023-01-14 23:13:27 +0900

    feat: 예외 처리 위해 repo 수정

--- src/main/java/com/demo/repository/BookRoomRepo.java
+++ src/main/java/com/demo/repository/BookRoomRepo.java
@@ -2,17 +2,18 @@ package com.demo.repository;
 
 import com.demo.domain.BookRoomPlusBookDetails;
 import com.demo.domain.Bookroom;
-import com.demo.dto.RecommendBookFavoriteDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface BookRoomRepo extends JpaRepository<Bookroom, Long> {
+    @Query("SELECT br.bookId FROM Bookroom br WHERE br.userId = :userId")
+    List<Long> findBookroomId(@Param("userId") Long userId);
+
     @Query(value = "select br.bookroom_id, br.book_id, br.theme_color, br.theme_music_url, br.user_id, bd.book_title as Bookdetails "
             + "from Bookdetails bd JOIN Bookroom br ON br.book_id = bd.book_id "
             + "where br.user_id = :userId", nativeQuery = true)
