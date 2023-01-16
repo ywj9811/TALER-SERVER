d69ed0e548bcdf29671ed82a0b8b6a96b282e057
양원준 <97458548+ywj9811@users.noreply.github.com>
2023-01-16 21:53:27 +0900

    fix: findByUserIdAndBookId 반환 타입 수정

--- src/main/java/com/demo/repository/UserCharacterRepo.java
+++ src/main/java/com/demo/repository/UserCharacterRepo.java
@@ -4,7 +4,13 @@ import com.demo.domain.Usercharacter;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserCharacterRepo extends JpaRepository<Usercharacter, Long> {
-    Usercharacter findByUserIdAndBookId(Long userId, Long bookId);
+    Optional<Usercharacter> findByUserIdAndBookId(Long userId, Long bookId);
+    void deleteByUserIdAndBookId(Long userId, Long bookId);
 }
+/**
+ * Optional을 사용한 이유는 예외처리를 위해서 사용한 것으로 Usercharacter객체가 필요하면 결과.get()하면 됩니다
+ */
