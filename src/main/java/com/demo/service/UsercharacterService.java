4ad737c64d9fc0afdbbee7b646d54ae47a8aedc3
Hong Jun <rhkgkrwkhj@naver.com>
2023-01-16 18:35:43 +0900

    #usercharacter관련 작업, update문 필요

---
+++ src/main/java/com/demo/service/UsercharacterService.java
@@ -0,0 +1,37 @@
+package com.demo.service;
+
+import com.demo.domain.*;
+import com.demo.dto.*;
+import com.demo.repository.*;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+public class UsercharacterService{
+
+    private final UserCharacterRepo userCharacterRepo;
+    public Usercharacter getUsercharacter(Long userId, Long bookId) {
+        Usercharacter usercharacter = userCharacterRepo.findByUserIdAndBookId(userId, bookId);
+        return usercharacter;
+    }
+
+    public Usercharacter saveUsercharacter(UsercharacterDto usercharacterDto){
+        Usercharacter usercharacter = usercharacterDto.insertDtoToUsercharacter(usercharacterDto);
+        Usercharacter save = userCharacterRepo.save(usercharacter);
+        return save;
+    }
+
+    public Usercharacter updateUsercharacter(UsercharacterUpdateDto usercharacterupdateDto){
+        Usercharacter usercharacter = usercharacterupdateDto.updateDtoToUsercharacter(usercharacterupdateDto);
+        Usercharacter save = userCharacterRepo.save(usercharacter);
+        return save;
+    }
+
+}
