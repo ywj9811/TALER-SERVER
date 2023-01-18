bcc090a581bdfef04356c16d047cb150d364cbd7
양원준 <97458548+ywj9811@users.noreply.github.com>
2023-01-18 17:40:12 +0900

    feat: Bookroom조회 쿼리 추가

--- src/main/java/com/demo/repository/BookRoomRepo.java
+++ src/main/java/com/demo/repository/BookRoomRepo.java
@@ -13,6 +13,8 @@ import java.util.List;
 public interface BookRoomRepo extends JpaRepository<Bookroom, Long> {
     List<Bookroom> findByUserId(Long userId);
 
+    Bookroom findByBookIdAndUserId(Long bookId, Long userId);
+
     @Query("SELECT br.bookId FROM Bookroom br WHERE br.userId = :userId")
     List<Long> findBookroomId(@Param("userId") Long userId);
 
