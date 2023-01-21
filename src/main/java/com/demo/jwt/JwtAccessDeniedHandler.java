5e10b25bf09c11a36ad7d44b1044edd0140d2a15
zzansol <chsk9912@gmail.com>
2023-01-22 01:42:13 +0900

    feat : 401,403에러 Response 형식으로 변환

--- src/main/java/com/demo/jwt/JwtAccessDeniedHandler.java
+++ src/main/java/com/demo/jwt/JwtAccessDeniedHandler.java
@@ -1,5 +1,9 @@
 package com.demo.jwt;
 
+import com.demo.dto.response.Response;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.MediaType;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
@@ -7,12 +11,29 @@ import org.springframework.stereotype.Component;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import static com.demo.domain.responseCode.ResponseCodeMessage.*;
 
 @Component
 public class JwtAccessDeniedHandler implements AccessDeniedHandler {
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
         //필요한 권한이 없이 접근하려 할때 403
-        response.sendError(HttpServletResponse.SC_FORBIDDEN);
+        Response r = new Response("403 Forbidden",FORBIDDENEROORMESSAGE,FORBIDDENEROORCODE);
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        objectMapper.getFactory().configure(JsonGenerator.Feature.ESCAPE_NON_ASCII, true);
+
+        String res = objectMapper.writeValueAsString(r);
+        PrintWriter writer = response.getWriter();
+        writer.write(res);
+        writer.flush();
+
+
+
     }
 }
