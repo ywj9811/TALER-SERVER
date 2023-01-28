5dbe501db393f99dc2c89a4e5416d06da7b07e3c
Hong Jun <rhkgkrwkhj@naver.com>
2023-01-28 13:53:56 +0900

    login test

--- src/main/java/com/demo/config/SecurityConfig.java
+++ src/main/java/com/demo/config/SecurityConfig.java
@@ -41,7 +41,8 @@ public class SecurityConfig{
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http.csrf().disable();
+        /*
                 .exceptionHandling()
                 .accessDeniedHandler(jwtAccessDeniedHandler)
                 .authenticationEntryPoint(jwtAuthenticationEntryPoint)
@@ -61,42 +62,7 @@ public class SecurityConfig{
 
                 .and()
                 .apply(new JwtSecurityConfig(tokenProvider));
-
+*/
         return http.build();
-// SpringSecurity란, Spring기반의 애플리케이션의 보안(인증, 권한, 인가 등)을 담당하는 Spring 하위 프레임워크
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    /**
-     * SpringSecurity 설정
-     */
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        /**
-         .exceptionHandling()
-         .authenticationEntryPoint(jwtAuthenticationEntryPoint)
-         .accessDeniedHandler(jwtAccessDeniedHandler)
-
-         // 세션을 사용하지 않기 때문에 STATELESS로 설정
-         .and()
-         .sessionManagement()
-         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-
-         .and()
-         .authorizeRequests()
-         .antMatchers("/user/save").permitAll()
-         .antMatchers("/user/parent/save").permitAll()
-         .antMatchers("/user/login").permitAll()
-         .antMatchers("/user/parent/login").permitAll()
-         .anyRequest().authenticated()
-
-         .and()
-         .apply(new JwtSecurityConfig(tokenProvider));
-         **/
-        http.csrf().disable();  // CSRF 비활성화,
-        // REST API 서버는 stateless하게 개발하기 때문에 사용자 정보를 Session에 저장 안함
-        // jwt 토큰을 Cookie에 저장하지 않는다면, CSRF에 어느정도는 안전.
-
-        //csrf를 비활성화 하게 되면 실행시 인증을 하지 않아도 동작할 수 있다.
-        //원래 csrf에 의해서 delete, insert, update 등등에서 인증이 필요함
     }
 }
